17404f074588ae9d9ff772fa9297ba60ee616bd9
Paul Stupka <paul.stupka@sitecore.com>
2019-07-02 13:32:01 +0300

    Init

---
+++ src/Sitecore.Support.340568/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.340568")]
+[assembly: AssemblyProduct("Sitecore.Support.340568")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("FlatDataCrawler loses Security State and Site context when processing items in parallel")]
+
